a4e920b3a882772b5cf0ace2a2501aadb876b5f5
Lewin Xander Gulo <76929223+caclm10@users.noreply.github.com>
2024-10-24 23:41:11 +0700

    Create schema.ts

---
+++ server/db/schema.ts
@@ -0,0 +1,35 @@
+import { relations, sql } from "drizzle-orm";
+import { sqliteTable as table } from "drizzle-orm/sqlite-core";
+import * as t from "drizzle-orm/sqlite-core";
+
+export const User = table(
+    "users",
+    {
+        id: t.int().primaryKey({ autoIncrement: true }),
+        name: t.text(),
+        email: t.text().notNull().unique(),
+        password: t.text().notNull(),
+        createdAt: t.text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
+        updatedAt: t.text("updated_at").default(sql`(CURRENT_TIMESTAMP)`).$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
+    },
+);
+
+export const UserSession = table(
+    "user_sessions",
+    {
+        id: t.int().primaryKey({ autoIncrement: true }),
+        userId: t.int("user_id").references(() => User.id),
+        refreshToken: t.text().notNull().unique(),
+        createdAt: t.text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
+        updatedAt: t.text("updated_at").default(sql`(CURRENT_TIMESTAMP)`).$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
+    }
+)
+
+export const UserRelations = relations(User, ({ one }) => ({
+    UserSession: one(UserSession)
+}));
+
+export const UserSessionRelations = relations(UserSession, ({ one }) => ({
+    User: one(User, { fields: [UserSession.userId], references: [User.id] })
+}))
+
