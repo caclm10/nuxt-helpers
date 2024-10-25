378268b71f7bfd4ee1d5c542ed515f0468625055
Lewin Xander Gulo <76929223+caclm10@users.noreply.github.com>
2024-10-25 13:55:43 +0700

    Create auth-fetch.ts

---
+++ plugins/auth-fetch.ts
@@ -0,0 +1,43 @@
+import type { RegisterResponseData } from "~/types/response";
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const accessToken = useSessionStorage("access-token", "");
+
+    const _authFetch = $fetch.create({
+        baseURL: import.meta.env.baseURL,
+        onRequest({ options }) {
+            if (accessToken.value) {
+                options.headers.set("Authorization", `Bearer ${accessToken.value}`)
+            }
+        },
+        async onResponseError({ response }) {
+            if (response.status === 401 && response._data?.code === responseCode.JWT_EXPIRED) {
+                try {
+                    const res = await $fetch<HttpResponse<RegisterResponseData>>("/api/auth/refresh-token");
+                    accessToken.value = res.data!.accessToken;
+                } catch (error) {
+                    await nuxtApp.runWithContext(() => navigateTo("/login"));
+                }
+            } else {
+                await nuxtApp.runWithContext(() => navigateTo("/login"));
+            }
+        }
+    })
+
+    type AuthFetch = Parameters<typeof _authFetch>;
+    async function authFetch<D>(request: AuthFetch["0"], options?: AuthFetch["1"]) {
+        try {
+            return await _authFetch<D>(request, options);
+        } catch (error) {
+            if (isHttpError(error) && error.data.code === responseCode.JWT_EXPIRED) {
+                return await authFetch(request, options);
+            }
+        }
+    }
+
+    return {
+        provide: {
+            authFetch
+        }
+    }
+})  
